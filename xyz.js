93adfc2cbecaea2c31ac5264afaa88cb23d7dec5
Davenson Lombard <lombard.davenson@gmail.com>
2018-06-12 12:14:25 -0400

    merge

--- xyz.js
+++ xyz.js
@@ -2,13 +2,10 @@ const doclient = require('./index').init(process.env.DO_API_TOKEN)
 getActions()
 
 function getActions() {
-<<<<<<< HEAD
   doclient.sshKeys.destroyByName("Settler.Cloud").then((x) => {
     console.log(x)
   }).catch(e => { console.error(e) })
   /*
-=======
->>>>>>> 943784f771324858a79521e1282963fede0c00cb
   var cursor = doclient.droplets.get()
 
   return cursor.next()
